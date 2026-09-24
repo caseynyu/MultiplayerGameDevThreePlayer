635f7855f72528c4c83277f4f33ddf6691a8e631
shrubino <rubino.stephen@gmail.com>
2026-09-23 22:12:37 -0400

    Walljump and ice blocks
    added those to player controller

--- Assets/Tarodev 2D Controller/_Scripts/PlayerController.cs
+++ Assets/Tarodev 2D Controller/_Scripts/PlayerController.cs
@@ -22,8 +22,7 @@ namespace TarodevController
         private bool _cachedQueryStartInColliders;
 
         public Gamepad currentGamepad;
-        public GameManager gameManager; 
-            
+        public GameManager gameManager;
 
         #region Interface
 
@@ -105,7 +104,6 @@ namespace TarodevController
 
                 jumpDown |= currentGamepad.buttonSouth.wasPressedThisFrame;
                 jumpHeld |= currentGamepad.buttonSouth.isPressed;
-                Debug.Log(jumpDown);
             }
 
             _frameInput = new FrameInput
@@ -150,6 +148,9 @@ namespace TarodevController
         
         private float _frameLeftGrounded = float.MinValue;
         private bool _grounded;
+		private bool _onIce;
+		private bool _onWall;
+		private int _wallDirection;
         //check if you're on a wall and which way is away from that wall
         private bool _onWall;
         private int _wallDirection;
@@ -159,26 +160,27 @@ namespace TarodevController
             Physics2D.queriesStartInColliders = false;
 
             // Ground and Ceiling
-            bool groundHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.down, _stats.GrounderDistance, ~_stats.PlayerLayer);
-            bool ceilingHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.up, _stats.GrounderDistance, ~_stats.PlayerLayer);
-            //wall
-            bool rightWallHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size,_col.direction, 0, Vector2.right, _stats.GrounderDistance, ~_stats.PlayerLayer);
-            bool leftWallHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size,_col.direction, 0, Vector2.left, _stats.GrounderDistance, ~_stats.PlayerLayer);
-            _onWall = !groundHit && (rightWallHit || leftWallHit);
-            
-            if (rightWallHit)
-            {
-                _wallDirection = 1;
-            }
-            else if (leftWallHit)
-            {
-                _wallDirection = -1;
-            }
-            else
-            {
-                _wallDirection = 0;
-            }
-            // Hit a Ceiling
+            RaycastHit2D groundHit = Physics2D.CapsuleCast(
+    _col.bounds.center,
+    _col.size,
+    _col.direction,
+    0,
+    Vector2.down,
+    _stats.GrounderDistance,
+    ~_stats.PlayerLayer
+);
+
+bool groundDetected = groundHit.collider != null;
+//check if you're on ice:
+_onIce =
+    groundDetected &&
+    groundHit.collider.gameObject.CompareTag("Ice");
+//Ceiling detection
+ bool ceilingHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.up, _stats.GrounderDistance, ~_stats.PlayerLayer);
+//walls			
+bool leftWallHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.left, _stats.GrounderDistance, ~_stats.PlayerLayer); 
+			bool rightWallHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.right, _stats.GrounderDistance, ~_stats.PlayerLayer);    
+			// Hit a Ceiling
             if (ceilingHit) _frameVelocity.y = Mathf.Min(0, _frameVelocity.y);
 
             // Landed on the Ground
